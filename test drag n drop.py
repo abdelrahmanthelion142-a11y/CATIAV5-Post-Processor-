119f51421438fafc9156a61c5244eb31fb80a6fd
karlo-c277 <karlo.ugrin@gmail.com>
2026-04-07 12:07:12 +0200

    Update launch configuration and enhance error handling in drag and drop script

--- test drag n drop.py
+++ test drag n drop.py
@@ -2,30 +2,33 @@
 import os
 import sys
 
-# check if file was dropped on the script
+# check if file was first argument
 if len(sys.argv) < 2:
-    print("Odaberi i povuci .txt datoteku APT koda na tekst.")
-    input("Klikni Enter za dalje...")
-    exit()
+    print("Nije predana datoteka.")
+    # input("Klikni Enter za dalje...")
+    exit(1000)
 
 # this is the file you dropped
 input_file = sys.argv[1]
+machine = sys.argv[2] if len(sys.argv) > 2 else None
 
 # provjeriti da li postoji file
 if not os.path.exists(input_file):
     print("Nepostojeća datoteka.")
-    input("Klikni Enter za dalje...")
-    exit()
+    # input("Klikni Enter za dalje...")
+    exit(1001)
 
 
 # provjeriti da li je file tekstualni
 if not os.path.isfile(input_file):
     print("Neispravna datoteka.")
-    input("Klikni Enter za dalje...")
-    exit()
+    #input("Klikni Enter za dalje...")
+    exit(1002)
 
 print("Datoteka učitana:", input_file)
 print("Pretraživanje linija\n")
+
+
 
 # provjeriti da li je zaista tekstualna datoteka
 try:
@@ -33,15 +36,17 @@ try:
         for line in f:
             line = line.strip()
             print(line)
+
             
             
 except UnicodeDecodeError:
     print("Nevaljana vrsta datoteke. Molimo odaberite tekstualnu datoteku.")
-    input("Klikni Enter za dalje...")
-    exit()
+    #input("Klikni Enter za dalje...")
+    exit(1003)
 except Exception as e:
     print(f"Greška prilikom čitanja datoteke: {e}")
-    input("Klikni Enter za dalje...")
-    exit()
+    #input("Klikni Enter za dalje...")
+    exit(1004)
 
-input("\nGotovo. Klikni Enter za zatvaranje...")
+print("\nGotovo.")
+exit(0)
